128e932c1e403815254a0d606230e98620ac6f41
dongtiandexue <huawei_code@163.com>
2019-03-01 20:45:43 +0800

    feign结合hystrix实战

---
+++ hystrix-feign-consumer/src/main/java/com/dtdx/hystrix/hystrixfeignconsumer/HystrixFeignConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dtdx.hystrix.hystrixfeignconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixFeignConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixFeignConsumerApplication.class, args);
+    }
+
+}
